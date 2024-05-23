ab70cd7741486d305d6d8ecc6e23cff68379cbea
LebogangMputle <lebozikalala@gmail.com>
2024-05-23 15:20:53 +0200

    "Added logging of provinces and names, created matching province names, and converted province names to uppercase"

--- scripts.js
+++ scripts.js
@@ -4,6 +4,20 @@ const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', '
 // A list of names:
 const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
+// Log each name and each province to the console
+provinces.forEach(province => console.log(province));
+names.forEach(name => console.log(names));
+
+// Log each name with a matching province in the format "Name (Province)"
+const matchingProvinces = provinces.filter((province, index) => names[index]);
+matchingProvinces.forEach((province, index) => console.log(`${names[index]} (${province})`));
+
+// Create a new array of province names in all uppercase
+const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+
+// Log the new array to the console
+console.log(uppercaseProvinces);
+
 // A list of products with prices:
 const products = [
   { product: 'banana', price: "2" },
